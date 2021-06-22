208a4852d9ec45624d9fc3d7a646381b6ed1495d
Olga Velichko <60892608+OVUSA@users.noreply.github.com>
2021-06-22 13:27:52 -0500

    Update Manager.java

--- src/Manager.java
+++ src/Manager.java
@@ -98,37 +98,7 @@ public class Manager implements State {
         return null;
     }
 }
-
 /*
-     public void report() {
-
-        System.out.println("User name : "+ getUserName());
-        LocalDate today = LocalDate.now();
-
-        System.out.println();
-        System.out.println( "Report for "+ today);
-        System.out.println();
-        System.out.println("Categories \t Duration:");
-
-        System.out.print(" Work   \t  ");
-        for (int i = 0 ; i < work.DurationWork.size(); i ++) {
-             work.DurationWork.get(i);
-            System.out.print( work.DurationWork.get(i));
-        }
-
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("00:mm:ss");
-       // Date date = new Date(workTimeDiff);
-       // String time = simpleDateFormat.format(date);
-
-
-       // element = 0;
-
-        System.out.println();
-        System.out.print(" Break   \t  ");
-        for (int i = 0 ; i < resting.DurationRest.size(); i ++) {
-             resting.DurationRest.get(i);
-            System.out.print(resting.DurationRest.get(i));
-        }
         SimpleDateFormat simpleDateFormat1 = new SimpleDateFormat("00:mm:ss");
         //Date date1 = new Date(element);
        // String time1 = simpleDateFormat1.format(date1);
