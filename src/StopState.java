2b7c5c232aef0933a53cefeb2dc6c4e90a4b2557
OlgaVelichko <ovelichko@luc.edu>
2021-06-21 19:40:13 -0500

    save time from StopState

--- src/StopState.java
+++ src/StopState.java
@@ -4,35 +4,31 @@ class StopState implements State{
 
     Tracker tracker;
     Manager storage = new Manager();
+
     StopState(Tracker tracker){
         this.tracker = tracker;
     }
 
-
     @Override
     public String onWork() {
-        tracker.changeState(new WorkState(tracker));
         LocalTime eTime = LocalTime.now();
-        String currentTime = tracker.timeFormatting(eTime);
         long start = System.currentTimeMillis();
         storage.startedWorking.add(start);
-        return "Started working at: "+ currentTime;
+        tracker.changeState(new WorkState(tracker));
+        return "Started working at: "+ storage.timeFormatting(eTime);
     }
 
     @Override
     public String onRest() {
-        tracker.changeState(new RestingState(tracker));
-       // tracker.setWorking(false);
         LocalTime eTime = LocalTime.now();
-        String currentTime = tracker.timeFormatting(eTime);
         long start = System.currentTimeMillis();
         storage.startedResting.add(start);
-        return "Started resting at "+ currentTime;
+        tracker.changeState(new RestingState(tracker));
+        return "Started resting at "+ storage.timeFormatting(eTime);
     }
 
     @Override
     public String onStop() {
-       // tracker.report();
         return "Inactive 'STOP' button";
     }
 
